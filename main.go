7e7e1e4af7a00caf16ef1a2feb42131fde15a68b
Amel Muminovic <amcenp@gmail.com>
2022-09-11 22:36:24 +0200

    Main setup

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello from main")
+	r := gin.Default()
+	PORT := os.Getenv("PORT")
+	fmt.Println("PORT IS", PORT)
+	r.GET("/health-check", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "works",
+		})
+	})
+	address := fmt.Sprintf("0.0.0.0:%s", PORT)
+	r.Run(address)
 }
